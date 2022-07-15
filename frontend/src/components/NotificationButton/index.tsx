c6d35d0205eb4ee358ce31bb423d03060686ebcc
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-07-15 18:54:15 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,14 @@
+import icon from '../../assets/notification-icon.svg' 
+// esse import vai funcionar como se fosse uma variavel, através do import 
+// eu vou procurar o que eu quero armazenar e dps eu coloco como atributo 
+// da tag... para isso eu uso o {atributo}.
+
+function NotificationButton(){
+  return (
+    <div className="dsmeta-red-btn">
+      <img src={icon} alt="Notificar" />
+    </div>
+  )
+}
+
+export default NotificationButton
