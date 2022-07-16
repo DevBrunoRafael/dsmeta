351dab967cdccdac0fa4e49bef4741f4e0bac1c9
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-07-16 13:58:44 -0300

    changing database (h2 -> Mysql)

---
+++ backend/src/main/java/com/devbrunorafael/dsmeta/entities/Sale.java
@@ -0,0 +1,28 @@
+package com.devbrunorafael.dsmeta.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String sellerName;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+}
