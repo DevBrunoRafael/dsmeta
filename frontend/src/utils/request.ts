479da571e6f8782858fdc9387cb8f9d1d1ebb69d
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-08-20 13:58:12 -0300

    ...

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,8 +1,7 @@
 export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
 
-// const armazena uma variavel chamad a VBURL que costuma ser uma varavel de ambiente 
-// que é configurada para o frontend, caso ela não exista é utilizado o local host
+/*
+* const stores a variable called VBURL which is usually a standard environment variable,
+* configured for the frontend, if it does not exist it is used on the local host.
+*/
 
-// ?? operador de coalescencia nula
-
-// na direita é o valor padrão, mas caso exista o valor da variavel de ambiente, ele será utilizado
